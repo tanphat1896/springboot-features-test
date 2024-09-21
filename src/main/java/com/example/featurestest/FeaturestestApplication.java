2d00079f2754cea1cdb8c23a9441327584c359f0
Niibbles <ngtanphat1896@gmail.com>
2024-09-21 12:46:39 +0700

    Add spring project

---
+++ src/main/java/com/example/featurestest/FeaturestestApplication.java
@@ -0,0 +1,13 @@
+package com.example.featurestest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FeaturestestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FeaturestestApplication.class, args);
+	}
+
+}
